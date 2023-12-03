16a30d850c2f411c451099e95a0c357e51878af3
Matheus Henrique <rodriguesm673@gmail.com>
2023-12-03 17:57:27 -0300

    .

--- src/bnl.rkt
+++ src/bnl.rkt
@@ -1,5 +1,61 @@
 #lang racket
 
+(define (parser tokens)
+  (cond
+   ((null? tokens) (error "Empty program"))
+   ((equal? (car tokens) 'function ) (parse-function (cdr tokens)))
+   (else (parser (cdr tokens)))
+  )
+)
+
+(define (parse-function tokens)
+  (let ((function-name (cadr tokens))
+        (function-args (caddr tokens))
+        (function-body (cdddr tokens)))
+    (list 'function function-name function-args function-body)))
+
+(define (eval ast env)
+  (cond 
+    ((equal? (car ast) 'function) (eval-function (cdr ast) env))
+    ((equal? (car ast) 'vars ) (eval-vars (cdr ast) env))
+    ((equal? (car ast) 'begin) (eval-begin (cdr ast) env))
+    ((equal? (car ast) 'if) (eval-if (cdr ast) env))
+    ((equal? (car ast) 'return) (eval-return (cadr ast) env))
+  ))
+
+(define (eval-function ast env)
+  (let ((function-name (car ast))
+        (function-args (cadr ast))
+        (function-body (caddr ast)))
+    (define (function-eval args)
+      (let ((new-env (append (map (lambda (arg) (list arg (car args))) function-args) env)))
+        (eval function-body new-env)))
+    (list function-name function-eval)))
+
+(define (eval-vars ast env)
+ (let ((vars (car ast)))
+   (define (eval-var var)
+     (let ((var-name (car var))
+           (var-value (cadr var)))
+       (list var-name (eval var-value env))))
+   (map eval-var vars))
+)
+
+(define (eval-begin ast env)
+  (let ((expressions (car ast)))
+    (map (lambda (expr) (eval expr env)) expressions)))
+
+(define (eval-if ast env)
+  (let ((condition (car ast))
+        (then-expr (cadr ast))
+        (else-expr (caddr ast)))
+    (if (eval condition env)
+        (eval then-expr env)
+        (eval else-expr env))))
+
+(define (eval-return ast env)
+  (eval (car ast) env))
+
 (define (lexer code)
   (let ((tokens '())
         (current-token '())
@@ -32,17 +88,14 @@
     tokens))
 ;; Exemplo de uso:
 (define code-example
-  "(function f < a b >
-     vars c
+  "( function main < >
+     vars a b
+     b = 2 !
      begin
        a = 2 !
        if a gt b then b = 0 fi
        return 0 !
      end
-     function main < >
-     begin
-       f < 1 2 > !
-       delete f @ 2 !
-       f < 5 3 > !)")
+   )")
 
-(display (lexer (string->list code-example)))
+(display (eval (parser (lexer (string->list code-example))) '()))
